23f43b83849a77c2083626f90a222ab850cbcf89
EoBryanDev <mauriciobryan1998@gmail.com>
2023-08-25 20:39:37 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={`${styles.container} ${inter.className}`}>
+      <Head>
+        <title>Tasks+ | Organize your tasks easily </title>
+      </Head>
+    
+    </div>
+  )
+}
