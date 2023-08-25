d409d03b5c26782dfb26a756b737dbd3d4887817
EoBryanDev <mauriciobryan1998@gmail.com>
2023-08-25 20:59:29 -0300

    Build Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/home.module.css";
+import heroImg from "../../public/assets/hero.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -11,7 +12,20 @@ export default function Home() {
       <Head>
         <title>Tasks+ | Organize your tasks easily </title>
       </Head>
-    
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt='Tasks Logo'
+            src={heroImg}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>
+          App made up to you organize all your tasks 
+        </h1>
+      </main>
     </div>
-  )
+  );
 }
