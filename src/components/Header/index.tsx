abee831a57bc801e90abc4592c83567fe4519705
EoBryanDev <mauriciobryan1998@gmail.com>
2023-08-26 15:27:44 -0300

    Creating Login Functions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,45 @@
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
+  const { data: session, status } = useSession();
   return (
     <header className={styles.header}>
       <section className={styles.content}>
         <nav className={styles.nav}>
           <Link href='/'>
-            <h1 className={styles.logo}>Tasks <span>+</span></h1>
+            <h1 className={styles.logo}>
+              Tasks <span>+</span>
+            </h1>
           </Link>
-          <Link href='/dashboard' className={styles.link}>My Dashboard
-          </Link>
+          {(session?.user && (
+            <Link href='/dashboard' className={styles.link}>
+              My Dashboard
+            </Link>
+          )) ||
+            ""}
         </nav>
-       
 
-        <button type="button" className={styles.loginButton}>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button
+            type='button'
+            className={styles.loginButton}
+            onClick={() => signOut()}
+          >
+            <span>Hello {session?.user?.name}</span>
+          </button>
+        ) : (
+          <button
+            type='button'
+            className={styles.loginButton}
+            onClick={() => signIn("google")}
+          >
             <span>Login</span>
-        </button>
-     
+          </button>
+        )}
       </section>
     </header>
   );
